9861d5ad855608559c6493a1d3fdf4ca276ad125
M4rcoAurelio <shadowfeetfartren@gmail.com>
2024-09-15 12:52:14 -0300

    Prisma e Mongodb posto

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
+app.use(express.json);
+
+const prisma = new PrismaClient();
 
 app.get('/conteudo', (req, res) =>
 {
 
-    
+    res.send("Deu bom!");
 
 })
